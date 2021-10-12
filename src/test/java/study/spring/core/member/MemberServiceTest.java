2ed73bf97bec02f862c9eee128d411863078cccb
kkambee <bok03220@gmail.com>
2021-10-13 03:17:05 +0900

    feat: IOC 없이 DI 하는 회원 도메인 개발 (OCP, DIP 위반)

---
+++ src/test/java/study/spring/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package study.spring.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemberServiceTest {
+
+    private final MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member foundMember = memberService.findMember(member.getId());
+
+        // then
+        Assertions.assertThat(member).isEqualTo(foundMember);
+    }
+}
